21eeb8ccce192dfedcb831e0a696b5c8817f098e
unknown <gsemily22@gmail.com>
2025-04-24 12:02:14 +0900

    Lv 2

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+        MenuItem menu;
+        List<MenuItem> menuItems = new ArrayList<>();
+
+        menu = new MenuItem("Shackburger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+        menuItems.add(menu);
+        menu = new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+        menuItems.add(menu);
+        menu = new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+        menuItems.add(menu);
+        menu = new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거");
+        menuItems.add(menu);
+
         Scanner scanner = new Scanner(System.in);
 
-        String [] menu = {"1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거",
-                "2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거",
-                "3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거",
-                "4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거",
-                "0. 종료      | 종료"
-        };
-
         System.out.println("[ SHAKESHACK MENU ]");
-        for(String s:menu){
-            System.out.println(s);
+        int number = 1;
+        for(MenuItem s:menuItems){
+            System.out.print(number + ". ");
+            System.out.println(s.show());
+            number++;
         }
+        System.out.println("0. 종료");
 
         while(true){
             System.out.print("> ");
-            String answer = scanner.nextLine();
+            int answer = scanner.nextInt();
 
             switch(answer){
-                case "1" -> System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-                case "2" -> System.out.println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-                case "3" -> System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-                case "4" -> System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
-                case "0" -> {
+                case 1 -> System.out.println("메뉴 1번입니다 : ShackBurger");
+                case 2 -> System.out.println("메뉴 2번입니다 : SmokeShack");
+                case 3 -> System.out.println("메뉴 3번입니다 : Cheeseburger");
+                case 4 -> System.out.println("메뉴 4번입니다 : Hamburger");
+                case 0 -> {
                     System.out.println("프로그램을 종료합니다");
                     return;
                 }
+                default -> System.out.println("존재하지 않는 주문번호입니다.");
             }
         }
     }
--- src/MenuItem.java
+++ src/MenuItem.java
@@ -1,2 +1,16 @@
 public class MenuItem {
+    private String name;
+    private String description;
+    private double price;
+
+    //생성자
+    MenuItem(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String show() {
+        return String.format("%-15s | W %.1f | %s", name, price, description);
+    }
 }
